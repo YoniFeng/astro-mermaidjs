7799b4d6a5849c4bdb5e857e0ea461a30eaa52c9
Yoni Feigelson <yfeigelson@gmail.com>
2023-03-22 10:11:10 +0200

    update

--- dist/index.mjs
+++ dist/index.mjs
@@ -12,19 +12,18 @@ import $hgUW1$playwright from "playwright";
 async function $3b3adf5e97fa5fd8$export$2e2bcd8739ae039({ config: config , code: code  }) {
     const browser = await (0, $hgUW1$playwright).chromium.launch({
         args: [
-            "--no-sandbox",
-            "--no-first-run"
+            "--no-sandbox"
         ]
     });
     const page = await browser.newPage();
-    const content = await (0, $hgUW1$nodefspromises).readFile((0, $hgUW1$nodepath).join($hgUW1$cwd(), "node_modules/mermaid/dist/mermaid.js"), "utf8");
+    const content = await (0, $hgUW1$nodefspromises).readFile((0, $hgUW1$nodepath).join($hgUW1$cwd(), "node_modules/mermaid/dist/mermaid.min.js"), "utf8");
     await page.addScriptTag({
         content: content
     });
-    const result = await page.evaluate(({ config: config , code: string  })=>{
-        window.mermaid.initialize(config);
+    const result = await page.evaluate(([diagramCode])=>{
+        window.mermaid.initialize({});
         try {
-            /* Render the mermaid diagram */ const svgCode = window.mermaid.mermaidAPI.render("diagram", code);
+            /* Render the mermaid diagram */ const svgCode = window.mermaid.mermaidAPI.render("diagram", diagramCode);
             return {
                 status: "success",
                 svgCode: svgCode
@@ -36,10 +35,9 @@ async function $3b3adf5e97fa5fd8$export$2e2bcd8739ae039({ config: config , code:
                 message: error.message
             };
         }
-    }, {
-        config: config,
-        code: code
-    });
+    }, [
+        code
+    ]);
     await browser.close();
     if (result.status === "success" && typeof result.svgCode === "string") return result.svgCode;
     return false;
